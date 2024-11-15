2ef3e88044b326c03dffddcd0587c13ea750ae25
Riza "Risky" Resnick <riza@nysom.com>
2024-11-15 12:38:13 -0800

    Simplify default settings

--- Core/ScriptableObjects/AnarchySettings.cs
+++ Core/ScriptableObjects/AnarchySettings.cs
@@ -5,8 +5,8 @@ namespace AnarchyConstructFramework.Core.ScriptableObjects
     [CreateAssetMenu(fileName = "Data", menuName = "Anarchy/Create Settings", order = 1)]
     public class AnarchySettings : ScriptableObject
     {
-        public string RootNamespace = "AnarchyConstructFramework";
-        public string PathToAnarchyConstructFramework = "Assets/Anarchy-Construct-Framework";
-        public string PathToConstructs = "Assets/Anarchy-Construct-Framework/_Constructs";
+        public string RootNamespace = "Anarchy";
+        public string PathToAnarchyConstructFramework = "Assets/Anarchy";
+        public string PathToConstructs = "Assets/_Constructs";
     }
 }
